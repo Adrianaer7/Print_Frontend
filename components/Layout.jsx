e3d9b077dbf7018deb83c24c582c1d704b974c86
Adrian Roldán <adrianaer7@hotmail.com>
2022-05-09 17:17:11 -0300

    SD

---
+++ components/Layout.jsx
@@ -0,0 +1,18 @@
+import Head from "next/head"
+import Header from "./Header"
+import Footer from "./Footer"
+import favicon from "../public/favicon.ico"
+const Layout = ({children, pagina}) => {
+  return (
+    <>
+      <Head>
+        <title>The Print Company Argentina - {pagina}</title>
+        <meta name="description" content="Sitio web de impresiones de libros académicos desde Gualeguaychú a todo el país"/>
+        <link rel="shortcut icon" type="image/x-icon" href={favicon.src}/>
+      </Head>
+      {children}
+    </>
+  )
+}
+
+export default Layout
---
+++ pages/libros.jsx
@@ -0,0 +1,26 @@
+import Layout from "../components/Layout"
+
+const Libros = ({libros}) => {
+
+    
+
+  return (
+    <Layout pagina="Todos los libros">
+      <div>Libros</div>
+    </Layout>
+  )
+}
+
+export default Libros
+
+
+export async function getServerSideProps() {
+    const url = `${process.env.API_URL}/libros`
+    const respuesta = await fetch(url)
+    const libros = await respuesta.json()
+    return {
+      props: {
+        libros
+      }
+    }
+  }
